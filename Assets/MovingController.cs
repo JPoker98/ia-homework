a8c01b562065890ca5624ca46fdf0aba3ae8fc79
JPoker98 <jocesmar@gmail.com>
2018-09-25 06:56:36 +0200

    IA exercises

---
+++ Assets/MovingController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingController : MonoBehaviour {
+
+    Vector3 initialTransform = new Vector3 (0,0,0);
+    public Transform fixedTransform;
+
+    public enum PossibleMoves
+    {
+        StraightForward,
+        Rotate,
+        PatrolOne,
+        PatrolSeveral,
+    }
+    public PossibleMoves currentMove;
+    public float charSpeed;
+    public float charRotationSpeed;
+    public GameObject charGoalPosition;
+    public GameObject[] charGoalPoints;
+
+    bool straightForward;
+    bool rotate;
+    bool patrol;
+   
+
+    //Patrolling variables
+    public int nextPointToCheck = 1;
+    public Vector3[] positions; 
+
+    // Use this for initialization
+    void Start() {
+        initialTransform = transform.position;
+
+        if (currentMove == PossibleMoves.StraightForward)
+        {
+            straightForward = true;
+        }
+        else if (currentMove == PossibleMoves.Rotate)
+        {
+            rotate = true;
+        }
+        else if (currentMove == PossibleMoves.PatrolOne)
+        {
+            Vector3[] positions = new Vector3[2];
+            positions[0] = initialTransform;
+            positions[1] = charGoalPosition.transform.position;
+            patrol = true;
+        }
+        else
+        { 
+            //Includes origin as extra point
+            positions = new Vector3[charGoalPoints.Length+1];
+            positions[0] = initialTransform;
+            for (int i = 1; i < positions.Length; i++ )
+            {
+                positions[i] = charGoalPoints[i - 1].transform.position;
+            }
+            patrol = true;
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (straightForward)
+        {
+            MoveStraightForward();
+        }
+        else if (rotate)
+        {
+            RotateItself();
+        }
+        else if (patrol)
+        {
+ 
+            Patrol(positions);
+        }
+
+    }
+
+    public void MoveStraightForward()
+    {
+        fixedTransform.position += Vector3.forward * Time.deltaTime * charSpeed;
+    }
+
+    public void RotateItself()
+    {
+        fixedTransform.Rotate(Vector3.up * Time.deltaTime * charRotationSpeed);
+    }
+
+    public void Patrol(Vector3[] positions)
+    {
+        Debug.Log(positions.Length + ":"+positions[0]+""+positions[1]);
+        fixedTransform.position = Vector3.MoveTowards(transform.position, positions[nextPointToCheck], charSpeed * Time.deltaTime);
+
+        //Checks if it has arrived and change objective if so
+        if (Vector3.Distance(transform.position, positions[nextPointToCheck]) < 0.001f )
+        {
+            ++nextPointToCheck;
+            nextPointToCheck %= positions.Length;
+            fixedTransform.LookAt(positions[nextPointToCheck]);
+
+        }
+
+       
+    }
+}
---
+++ Assets/WallGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : MonoBehaviour
+{
+
+    public GameObject brick;
+    public Vector3 brickDimensions;
+    public int rows;
+    public int columns;
+
+    public bool zigzag;
+    public bool delay;
+    public float delayBetweenBricks;
+
+    bool built = false;
+    // Use this for initialization
+    void Start()
+    {
+        brickDimensions = brick.GetComponent<MeshFilter>().mesh.bounds.size;
+        Vector3 position = new Vector3(0, 0.5f, 0);
+        //Instantiate(brick, position, Quaternion.identity, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!built)
+        {
+            built = true;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (zigzag)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            Vector3 zigzagPosition = new Vector3(j * brickDimensions.x - brickDimensions.x / 2, 0.5f + i * brickDimensions.y, 0);
+
+                            Instantiate(brick, zigzagPosition, Quaternion.identity, transform);
+                            
+                        }
+                        else
+                        {
+                            Vector3 position = new Vector3(j * brickDimensions.x, 0.5f + i * brickDimensions.y, 0);
+
+                            Instantiate(brick, position, Quaternion.identity, transform);
+                        }
+
+
+                    }
+                    else
+                    {
+                        Vector3 position = new Vector3(j * brickDimensions.x, 0.5f + i * brickDimensions.y, 0);
+
+                        Instantiate(brick, position, Quaternion.identity, transform);
+                    }
+
+
+
+                }
+            }
+        }
+    }
+
+}
+
+
